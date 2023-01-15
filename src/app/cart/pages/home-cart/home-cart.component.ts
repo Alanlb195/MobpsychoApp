15b246c2bd44b1c2d15103aad86e43942b7e2978
alanlb195 <alanlb195@gmail.com>
2023-01-15 16:36:42 -0500

    cart in progress

--- src/app/cart/pages/home-cart/home-cart.component.ts
+++ src/app/cart/pages/home-cart/home-cart.component.ts
@@ -1,25 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface CharacterCart {
+  idCharacter: number;
+  Name: string;
+  Price: number;
+  Quantity: number;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+var items: CharacterCart[] = [];
 
 @Component({
   selector: 'app-home-cart',
@@ -28,13 +16,16 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class HomeCartComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['Name', 'Price', 'Quantity'];
   columnsToDisplay: string[] = this.displayedColumns.slice();
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource = new MatTableDataSource(items);
 
   constructor() { }
 
   ngOnInit(): void {
+
+    
+
   }
 
   addColumn() {
